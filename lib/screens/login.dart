ca46b51f62f10f5a8620e177308bdda629958d16
duevanofairuz <duevanofairuz@gmail.com>
2025-06-08 20:46:03 +0700

    lazy styling login register page

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -51,37 +51,77 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-        centerTitle: true,
-      ),
+      // backgroundColor: const Color(0xFFDEFFE7),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.fromLTRB(64, 128, 64, 64),
         child: Center(
           child: ListView(
             children: [
               const SizedBox(height: 48),
-              Icon(Icons.lock_outline, size: 100, color: Colors.blue[200]),
+              // Icon(Icons.lock_outline, size: 100, color: Colors.blue[200]),
+              Image.asset(
+                'assets/LOGO.png',
+                height: 130,
+                width: 90,
+                // color: Colors.blue[200],
+              ),
               const SizedBox(height: 48),
               TextField(
                 controller: _emailController,
-                decoration: const InputDecoration(label: Text('Email')),
+                cursorColor: Color(0xFF046526),
+                style: TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  labelStyle: TextStyle(color: Color(0xFF046526)),
+                  hintStyle: TextStyle(color: Color(0xFF046526)),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black38),
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF046526)),
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                ),
               ),
+              const SizedBox(height: 12,),
               TextField(
                 controller: _passwordController,
+                cursorColor: Color(0xFF046526),
+                style: TextStyle(color: Colors.black),
                 obscureText: true,
-                decoration: const InputDecoration(label: Text('Password')),
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  labelStyle: TextStyle(color: Color(0xFF046526)),
+                  hintStyle: TextStyle(color: Color(0xFF046526)),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black38),
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF046526)),
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                ),
               ),
-              const SizedBox(height: 24),
+              const SizedBox(height: 12),
               _errorCode != ""
                   ? Column(
-                  children: [Text(_errorCode), const SizedBox(height: 24)])
+                  children: [Text(_errorCode), const SizedBox(height: 12)])
                   : const SizedBox(height: 0),
               OutlinedButton(
                 onPressed: signIn,
+                style: OutlinedButton.styleFrom(
+                  padding: EdgeInsets.all(16),
+                  backgroundColor: Color(0xFF046526),
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6), // Radius sudut
+                  ),
+                ),
                 child: _isLoading
-                    ? const CircularProgressIndicator()
-                    : const Text('Login'),
+                    ? const CircularProgressIndicator(color: Colors.white, )
+                    : const Text('Login', style: TextStyle(fontSize: 16),),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -89,7 +129,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   const Text('Don\'t have an account?'),
                   TextButton(
                     onPressed: navigateRegister,
-                    child: const Text('Register'),
+                    child: const Text('Register', style: TextStyle(color: Color(0xFF046526)),),
                   )
                 ],
               )
--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -51,38 +51,77 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Register'),
-        centerTitle: true,
-      ),
+      // backgroundColor: const Color(0xFFDEFFE7),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.fromLTRB(64, 128, 64, 64),
         child: Center(
           child: ListView(
             children: [
               const SizedBox(height: 48),
-              Icon(Icons.lock_outline, size: 100, color: Colors.blue[200]),
+              // Icon(Icons.lock_outline, size: 100, color: Colors.blue[200]),
+              Image.asset(
+                'assets/LOGO.png',
+                height: 130,
+                width: 90,
+                // color: Colors.blue[200],
+              ),
               const SizedBox(height: 48),
               TextField(
                 controller: _emailController,
-                decoration: const InputDecoration(label: Text('Email')),
+                cursorColor: Color(0xFF046526),
+                style: TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  labelStyle: TextStyle(color: Color(0xFF046526)),
+                  hintStyle: TextStyle(color: Color(0xFF046526)),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black38),
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF046526)),
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                ),
               ),
+              const SizedBox(height: 12,),
               TextField(
                 controller: _passwordController,
+                cursorColor: Color(0xFF046526),
+                style: TextStyle(color: Colors.black),
                 obscureText: true,
-                decoration: const InputDecoration(label: Text('Password')),
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  labelStyle: TextStyle(color: Color(0xFF046526)),
+                  hintStyle: TextStyle(color: Color(0xFF046526)),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black38),
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF046526)),
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                ),
               ),
-              const SizedBox(height: 24),
+              const SizedBox(height: 12),
               _errorCode != ""
                   ? Column(
-                children: [Text(_errorCode), const SizedBox(height: 24)],
-              )
+                  children: [Text(_errorCode), const SizedBox(height: 12)])
                   : const SizedBox(height: 0),
               OutlinedButton(
                 onPressed: register,
+                style: OutlinedButton.styleFrom(
+                  padding: EdgeInsets.all(16),
+                  backgroundColor: Color(0xFF046526),
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6), // Radius sudut
+                  ),
+                ),
                 child: _isLoading
-                    ? const CircularProgressIndicator()
-                    : const Text('Register'),
+                    ? const CircularProgressIndicator(color: Colors.white, )
+                    : const Text('Register', style: TextStyle(fontSize: 16),),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -90,7 +129,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                   const Text('Already have an account?'),
                   TextButton(
                     onPressed: navigateLogin,
-                    child: const Text('Login'),
+                    child: const Text('Login', style: TextStyle(color: Color(0xFF046526)),),
                   )
                 ],
               )
