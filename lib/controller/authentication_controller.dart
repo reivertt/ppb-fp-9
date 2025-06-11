7b3d66b7374de1d18bfcd71c230d13e0c8fe68e7
reivertt <billthehuman27@gmail.com>
2025-06-11 23:22:06 +0700

    refactor: add auth repository and controller

---
+++ lib/controller/authentication_controller.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ppb_fp_9/repository/authentication_repository.dart';
+
+class AuthenticationController extends GetxController {
+  static AuthenticationController get instance => Get.find();
+
+  final authRepository = Get.put(AuthenticationRepository());
+
+  final usernameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  RxBool isLoading = false.obs;
+  RxString errorCode = "".obs;
+
+  void register() async {
+    print("cont-called");
+    isLoading.value = true;
+    errorCode.value = "";
+
+    try {
+      print("cont-attempt");
+      final userCredential = await authRepository.registerWithEmailAndPassword(
+        emailController.text.trim(),
+        passwordController.text.trim(),
+      );
+
+      print("cont-back");
+      if (userCredential.user != null) {
+        await authRepository.saveUserData(userCredential.user!, usernameController.text.trim());
+        Get.offAllNamed('login');
+      }
+      print("cont-navigate");
+    } catch (e) {
+      print("cont-err");
+      errorCode.value = e.toString();
+    } finally {
+      print("cont-stop load");
+      isLoading.value = false;
+    }
+  }
+
+  Future<void> signIn() async {
+    print("cont-called");
+    isLoading.value = true;
+    errorCode.value = "";
+
+    try {
+      print("cont-attempt");
+      await authRepository.signInWithEmailAndPassword(
+        emailController.text.trim(),
+        passwordController.text.trim(),
+      );
+      print("cont-finished");
+      Get.offAllNamed('home');
+      print("cont-navigate");
+    } catch (e) {
+      print("cont-err");
+      errorCode.value = e.toString();
+    } finally {
+      print("cont-stop load");
+      isLoading.value = false;
+    }
+  }
+}
---
+++ lib/repository/authentication_repository.dart
@@ -0,0 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+class AuthenticationRepository extends GetxController {
+  static AuthenticationRepository get instance => Get.find();
+
+  final _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore.instance;
+
+  Future<UserCredential> registerWithEmailAndPassword(String email, String password) async {
+    try {
+      print("repo-try regis");
+      return await _auth.createUserWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      print("repo-firebase err");
+      throw Exception(e.code);
+    } catch (e) {
+      print("repo-err");
+      throw const FormatException('An unexpected error occurred.');
+    }
+  }
+
+  Future<UserCredential> signInWithEmailAndPassword(String email, String password) async {
+    try {
+      print("repo-try login");
+      return await _auth.signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      print("repo-firebase error");
+      throw Exception(e.code);
+    } catch (e) {
+      print("repo-error");
+      throw const FormatException('An unexpected error occurred.');
+    }
+  }
+
+  Future<void> saveUserData(User user, String username) async {
+    try {
+      print("repo-try create user");
+      await _firestore.collection('users').doc(user.uid).set({
+        'uid': user.uid,
+        'username': username,
+        'email': user.email,
+        'createdAt': Timestamp.now(),
+      });
+      print("repo-don");
+    } catch (e) {
+      print("repo-user err");
+      throw Exception('Could not save user data.');
+    }
+  }
+}
